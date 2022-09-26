711b32ea2bec44c3d5cd72dc435c8da30b80f7c9
OJ101003 <omarjaber9812@gmail.com>
2022-09-25 22:56:01 -0700

    Cleaned up code

--- converter.py
+++ converter.py
@@ -23,7 +23,6 @@ prefixes = {
     "None": 1
 }
 
-# Test
 time = { # Based on seconds
     "Hour": 3600,
     "Minute": 60,
@@ -46,48 +45,34 @@ def Length(value,originalPrefix,desiredPrefix):
 # 6 Spaces to print vertically
 exit = False
 while exit != True:
-    selectionMain = int(input("""What would you like to convert?\n1. Length(Working)\n2. Speed and Velocity(NOT WORKING)
-    \n3. Mass(NOT WORKING)\n4. Volume(NOT WORKING)\n5. Temperature(NOT WORKING)\n6. Time(NOT WORKING)\n7. Exit\n"""))
+    selectionMain = int(input("""1. Length(Working)\n2. Speed and Velocity(NOT WORKING)
+3. Mass(NOT WORKING)\n4. Volume(NOT WORKING)\n5. Time(NOT WORKING)\n6. Exit\n"""))
     while (selectionMain != 1 and selectionMain != 7):
-        print("Select option 1-7\n")
-        selectionMain = int(input("""What would you like to convert?\n1. Length\n2. Speed and Velocity
-        \n3. Mass\n4. Volume\n5. Temperature\n6. Time\n7. Exit\n"""))
+        selectionMain = int(input("""1. Length\n2. Speed and Velocity
+3. Mass\n4. Volume\n5. Time\n6. Exit\n"""))
     if (selectionMain == 1):
-            value = int(input("What is the value of the original number? "))
-            counter = 1
-            for i in prefixes:
-                print(str((counter)) +": "+ i)
-                counter += 1
-            originalPrefixSelection = int(input("What is the original prefix: "))
-            while (originalPrefixSelection not in range(1, 11)):
-                originalPrefixSelection = int(input("Please enter a number from 1 to 10: "))
-            desiredPrefix = int(input("What is your desired prefix: "))
-            while (desiredPrefix not in range(1, 11)):
-                desiredPrefix = int(input("Please enter a number from 1 to 10: "))
-            originalPrefixSelection = prefixKeys[originalPrefixSelection]
+            value = int(input("Original Number: "))
+            for x, i in enumerate(prefixes):
+                print(str((x+1)) +": "+ i)
+            originalPrefix = input("Original Prefix: ")
+            while (originalPrefix not in range(1, 11)) or (type(originalPrefix) != int ):
+                for x, i in enumerate(prefixes):
+                    print(str((x+1)) +": "+ i)
+                originalPrefix = int(input("Original Prefix: "))
+            desiredPrefix = input("Desired Prefix: ")
+            while (desiredPrefix not in range(1, 11)) or type(desiredPrefix) != int :
+                for x, i in enumerate(prefixes):
+                    print(str((x+1)) +": "+ i)
+                desiredPrefix = int(input("Desired Prefix: "))
+            originalPrefix = prefixKeys[originalPrefix]
             desiredPrefix = prefixKeys[desiredPrefix]
             if (desiredPrefix == "None"):
-                print(str(Length(value, originalPrefixSelection, desiredPrefix)) +" meters")
+                print(str(Length(value, originalPrefix, desiredPrefix)) +" meters")
             else:
-                print(str(Length(value, originalPrefixSelection, desiredPrefix)) +" "+ desiredPrefix +"meters")
-    if (selectionMain == 7):
+                print(str(Length(value, originalPrefix, desiredPrefix)) +" "+ desiredPrefix +"meters")
+    if (selectionMain == 6):
         exit = True
         break
-        
-        
-
-#elif (selectionMain == 2):
-    #Speed and velocity code
-#elif (selectionMain == 3):
-    #Mass code
-#elif (selectionMain == 4):
-    #Volume code
-#elif (selectionMain == 5):
-    #Temp
-#elif (selectionMain == 6):
-    #Time
-
-
 """
 The plan for SpeedAndVelocity is to have it call to the distance and time method seperately
 in order to keep code simple
